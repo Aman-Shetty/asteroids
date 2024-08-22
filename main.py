af949bf0f707368d403eb961721ec43756b491e8
Aman-Shetty <amanshetty370@gmail.com>
2024-08-22 19:28:31 +0530

    Update: Added Player Sprites and its functions

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import pygame
+from pygame.display import update
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     disp = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for object in updatable:
+            object.update(dt)
+
         pygame.Surface.fill(disp, (0,0,0))
+        for object in drawable:
+            object.draw(disp)
         pygame.display.flip()
+        dt = clock.tick(60) /1000
+
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
